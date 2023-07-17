995db61acda23466617b66d84fd03c3692d233fc
leroysb <leroybuliro@gmail.com>
2023-07-18 00:55:58 +0300

    task10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ module 10-update_topics """
-import pymongo
 from pymongo.collection import Collection
 from typing import List
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]):
+def update_topics(mongo_collection: Collection, name: str, topics):
     """ changes all topics of a school document based on the name """
     mongo_collection.update_many(
         {"name": name},

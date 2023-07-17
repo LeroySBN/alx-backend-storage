3626ac5bffe5c860872b9f77ad2072f65d86ed14
leroysb <leroybuliro@gmail.com>
2023-07-17 23:14:55 +0300

    task9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,7 +5,7 @@ module 8-all
 from pymongo import MongoClient
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection) -> list:
     """lists all documents in a collection"""
     if mongo_collection is None:
         return []
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection based on kwargs
     """
-    return mongo_collection.insert(kwargs)
+    return mongo_collection.insert_many(kwargs)

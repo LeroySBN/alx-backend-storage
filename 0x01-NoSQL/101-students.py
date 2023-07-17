d315c251d01649f0be707b67fe9ec1a1fe6b6e51
leroysb <leroybuliro@gmail.com>
2023-07-18 01:21:23 +0300

    task14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -10,20 +10,3 @@ def top_students(mongo_collection):
     """
     returns all students sorted by average score
     """
-    pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {"_id": "$_id", "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": ASCENDING}}
-    ]
-    result = mongo_collection.aggregate(pipeline)
-    return list(result)
-
-client = MongoClient('mongodb://localhost:27017')
-collection = client.my_db.my_collection
-top_students_list = top_students(collection)
-
-for student in top_students_list:
-    pprint(student["_id"], student["name"], student["scores"])
-
-for student in top_students_list:
-    pprint(student["_id"], student["name"], "=>", student["averageScore"])

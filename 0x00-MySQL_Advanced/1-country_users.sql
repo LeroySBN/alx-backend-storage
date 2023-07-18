70530b0167d7135db8f21feb307cb67ef5c6fa87
leroysb <leroybuliro@gmail.com>
2023-07-18 13:59:53 +0300

    task1bugfix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table
 DROP TABLE IF EXISTS users;
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),

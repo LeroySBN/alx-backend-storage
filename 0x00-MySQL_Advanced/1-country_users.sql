b5745567ce75d6b28d42234ed17fff529ed8ad58
leroysb <leroybuliro@gmail.com>
2023-07-18 11:02:26 +0300

    bug fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a table
-CREATE TABLE users (
+CREATE TABLE users IF NOT EXISTS (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),

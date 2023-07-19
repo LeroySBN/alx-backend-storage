ee2fa29e41de87680389c5cf12415b4ce118d7f7
leroysb <leroybuliro@gmail.com>
2023-07-20 01:03:00 +0300

    task3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,22 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Method call_history
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Method wrapper
+        """
+        self._redis.rpush(method.__qualname__ + ":inputs", str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(method.__qualname__ + ":outputs", str(result))
+        return result
+    return wrapper
+
+
 class Cache:
     """
     Class Cache
@@ -35,6 +51,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Method store

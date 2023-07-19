5045a7c1cafa639e5e37e1d81e9f865a745cc302
leroysb <leroybuliro@gmail.com>
2023-07-19 19:05:58 +0300

    task6 bug fix

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
-delimiter //
+DELIMITER //
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
@@ -28,4 +28,4 @@ BEGIN
     
 END //
 
-delimiter ;
+DELIMITER ;

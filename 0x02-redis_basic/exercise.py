e3634372911c589a7abab40e6f4f4a70f89d9583
leroysb <leroybuliro@gmail.com>
2023-07-19 23:59:06 +0300

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        getter function for key
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """
+        Method get_str
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Method get_int
+        """
+        return self.get(key, int)

8d3e319194a855727c2d90d0392fd93c72c335bd
leroysb <leroybuliro@gmail.com>
2023-07-20 01:08:49 +0300

    task4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -83,3 +83,18 @@ class Cache:
         Method get_int
         """
         return self.get(key, int)
+
+
+def replay(method: Callable):
+    """
+    Method replay
+    """
+    r = redis.Redis()
+    method_name = method.__qualname__
+    count = r.get(method_name).decode('utf-8')
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+    print("{} was called {} times:".format(method_name, count))
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(method_name, i.decode('utf-8'),
+                                     o.decode('utf-8')))

c3d9bc3c0bc58faa65f96a51fb68afbf199428f7
leroysb <leroybuliro@gmail.com>
2023-07-20 02:46:33 +0300

    task5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -31,11 +31,12 @@ def get_page(url: str) -> str:
     """
     Method that takes in a URL and returns the HTML content of the URL
     """
-    import requests
-    res = requests.get(url)
-    key = f"count:{url}"
-    # if cache.get(key):
-    #     cache.incr(key)
-    # else:
-    #     cache.set(key, 1)
-    return res.text
+    if url:
+        import requests
+        res = requests.get(url)
+        key = f"count:{url}"
+        # if cache.get(key):
+        #     cache.incr(key)
+        # else:
+        #     cache.set(key, 1)
+        return res.text
